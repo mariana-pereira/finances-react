4d38671654deeec734c99f768057b7b7a4868f2f
Mariana Pereira <marianna.22sp@gmail.com>
2020-06-19 11:38:19 -0300

    Create file for global styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => (
   <Router history={history}>
     <Routes />
+    <GlobalStyle />
   </Router>
 );
 
