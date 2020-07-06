e15d982b249a265e4948001dbacfc595f36603df
Mariana Pereira <marianna.22sp@gmail.com>
2020-07-06 10:25:40 -0300

    Add cards for main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -2,12 +2,32 @@ import React from 'react';
 
 import Header from '../../components/Header';
 
-import { Container } from './styles';
+import investmentIcon from '../../assets/icons/piggy.svg';
+import accountIcon from '../../assets/icons/money.svg';
+import invoiceIcon from '../../assets/icons/invoice.svg';
+
+import { Container, CardContainer, Card } from './styles';
 
 const Main: React.FC = () => (
   <Container>
     <Header />
-    <h1>Main</h1>
+    <CardContainer>
+      <Card>
+        <img src={accountIcon} alt="account" />
+        <h1>R$ 200</h1>
+        <span>Saldo em conta</span>
+      </Card>
+      <Card>
+        <img src={investmentIcon} alt="investments" />
+        <h1>R$ 200</h1>
+        <span>Saldo investido</span>
+      </Card>
+      <Card>
+        <img src={invoiceIcon} alt="invoice" />
+        <h1>R$ 200</h1>
+        <span>Total de faturas</span>
+      </Card>
+    </CardContainer>
   </Container>
 );
 
--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -3,3 +3,35 @@ import styled from 'styled-components';
 export const Container = styled.div`
   height: 100%;
 `;
+
+export const CardContainer = styled.div`
+  padding: 0 50px;
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Card = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: rgba(0, 0, 0, 0.15);
+  border-radius: 6px;
+
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: space-around;
+
+  img {
+    width: 32px;
+  }
+
+  h1 {
+    color: #444;
+  }
+
+  span {
+    color: #555;
+  }
+`;
