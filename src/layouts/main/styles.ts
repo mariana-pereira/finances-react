9e166e2a64aabab0aa6b98d0808b89ae02a4f9e3
Mariana Pereira <marianna.22sp@gmail.com>
2020-07-06 15:32:35 -0300

    Create layout for authenticated pages

---
+++ src/layouts/main/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  position: fixed;
+`;
+
+export const Content = styled.div`
+  height: 100%;
+  display: grid;
+  grid-template-columns: 280px 1fr;
+`;
+
+export const Main = styled.div`
+`;
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Layout from '../../layouts/main';
+
 import investmentIcon from '../../assets/icons/piggy.svg';
 import accountIcon from '../../assets/icons/money.svg';
 import invoiceIcon from '../../assets/icons/invoice.svg';
@@ -7,25 +9,27 @@ import invoiceIcon from '../../assets/icons/invoice.svg';
 import { Container, CardContainer, Card } from './styles';
 
 const Main: React.FC = () => (
-  <Container>
-    <CardContainer>
-      <Card>
-        <img src={accountIcon} alt="account" />
-        <h1>R$ 200</h1>
-        <span>Saldo em conta</span>
-      </Card>
-      <Card>
-        <img src={investmentIcon} alt="investments" />
-        <h1>R$ 200</h1>
-        <span>Saldo investido</span>
-      </Card>
-      <Card>
-        <img src={invoiceIcon} alt="invoice" />
-        <h1>R$ 200</h1>
-        <span>Total de faturas</span>
-      </Card>
-    </CardContainer>
-  </Container>
+  <Layout>
+    <Container>
+      <CardContainer>
+        <Card>
+          <img src={accountIcon} alt="account" />
+          <h1>R$ 200</h1>
+          <span>Saldo em conta</span>
+        </Card>
+        <Card>
+          <img src={investmentIcon} alt="investments" />
+          <h1>R$ 200</h1>
+          <span>Saldo investido</span>
+        </Card>
+        <Card>
+          <img src={invoiceIcon} alt="invoice" />
+          <h1>R$ 200</h1>
+          <span>Total de faturas</span>
+        </Card>
+      </CardContainer>
+    </Container>
+  </Layout>
 );
 
 export default Main;
