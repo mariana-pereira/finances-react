b55bde084bda67166b08d6efbb424e2b18c0c365
Mariana <marianadealmeidapereira@outlook.com>
2020-12-18 16:21:05 -0300

    test auth saga

--- src/__tests__/store/sagas/auth.spec.ts
+++ src/__tests__/store/sagas/auth.spec.ts
@@ -4,13 +4,22 @@ import MockAdapter from 'axios-mock-adapter';
 import api from '../../../services/api';
 
 import * as authActions from '../../../store/modules/auth/actions';
-import { signIn } from '../../../store/modules/auth/sagas';
+import { signIn, SignUp } from '../../../store/modules/auth/sagas';
 
 const apiMock = new MockAdapter(api);
 
-const action = {
+const signInAction = {
   type: '@auth/SIGN_IN_REQUEST',
   payload: {
+    email: 'test@test.com',
+    password: '123',
+  },
+};
+
+const signUpAction = {
+  type: '@auth/SIGN_UP_REQUEST',
+  payload: {
+    name: 'test',
     email: 'test@test.com',
     password: '123',
   },
@@ -22,17 +31,37 @@ describe('Auth saga', () => {
 
     apiMock.onPost('sessions').reply(200, { token: 'bearer 123456' });
 
-    await runSaga({ dispatch }, signIn, action).toPromise();
+    await runSaga({ dispatch }, signIn, signInAction).toPromise();
 
     expect(dispatch).toHaveBeenCalledWith(authActions.signInSuccess('bearer 123456'));
   });
 
-  it('should fail when api returns error', async () => {
+  it('should fail when api returns error in sign in', async () => {
     const dispatch = jest.fn();
 
     apiMock.onPost('sessions').reply(500);
 
-    await runSaga({ dispatch }, signIn, action).toPromise();
+    await runSaga({ dispatch }, signIn, signInAction).toPromise();
+
+    expect(dispatch).toHaveBeenCalledWith(authActions.signFailure());
+  });
+
+  it('should be able to sign up', async () => {
+    const dispatch = jest.fn();
+
+    apiMock.onPost('users').reply(200);
+
+    await runSaga({ dispatch }, SignUp, signUpAction).toPromise();
+
+    expect(dispatch).toHaveBeenCalledWith(authActions.signUpSuccess());
+  });
+
+  it('should fail when api returns error in sign up', async () => {
+    const dispatch = jest.fn();
+
+    apiMock.onPost('users').reply(500);
+
+    await runSaga({ dispatch }, SignUp, signUpAction).toPromise();
 
     expect(dispatch).toHaveBeenCalledWith(authActions.signFailure());
   });
