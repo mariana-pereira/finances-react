f0b73c8e902b7def8d4119ef71e804d00f90ac03
Mariana <marianadealmeidapereira@outlook.com>
2020-12-18 15:50:50 -0300

    test theme reducer

--- src/__tests__/store/reducers/theme.spec.ts
+++ src/__tests__/store/reducers/theme.spec.ts
@@ -4,6 +4,12 @@ import * as themeActions from '../../../store/modules/theme/actions';
 import dark from '../../../styles/themes/dark';
 
 describe('Theme reducer', () => {
+  it('DEFAULT', () => {
+    const state = reducer(undefined, {});
+
+    expect(state).toStrictEqual(INITIAL_STATE);
+  });
+
   it('APPLY_THEME', () => {
     const state = reducer(INITIAL_STATE, themeActions.applyTheme(dark));
 
